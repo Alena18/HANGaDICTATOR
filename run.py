dab002ebf35976eff2953494dab1bfc6f11ad96f
Alena18 <sun180983@gmail.com>
2022-12-12 19:08:23 +0000

    Add invitation to a game

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 import random
 import time
 
-
+print("\nWelcome to HANGaDICTATOR game by coffeebeanstudio1809@gmail.com")
+name = input("Enter your name:")
+print("Hello" + name + "Best of Luck!")
+time.sleep(3)
+print("The game is about to start!\nEnjoy!")
+time.sleep(3)
 
 
 # You can delete these comments, but do not change the name of this file
