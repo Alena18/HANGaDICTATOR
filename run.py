c6b98f80137df81597ff05b7a066d4a1034f85b7
Alena18 <sun180983@gmail.com>
2022-12-12 20:25:10 +0000

    Define main constants and variable

--- run.py
+++ run.py
@@ -1,13 +1,33 @@
 import random
 import time
-
+# invitation to a game
 print("\nWelcome to HANGaDICTATOR game by coffeebeanstudio1809@gmail.com")
 name = input("Enter your name:")
 print("Hello" + name + "Best of Luck!")
 time.sleep(3)
 print("The game is about to start!\nEnjoy!")
 time.sleep(3)
+# game
+# define variable
+def main():
+    global count
+    global display
+    global word
+    global guessed
+    global length
+    global play_game
+    words = ["freedom", "rights", "humanity", "democracy", "tolerance", 
+    "liberty", "equality", "justice", "honesty", "integrity", "fairness", 
+    "probity", "uprightness", "rectitude", "sincerity", "commonwealth", 
+    "emancipation","suffrage","election", "minority"]
+    word = random.choice(words)
+    length = len(word)
+    count = 0
+    display = "_"*length
+    guessed = []
+    play_game = ""
 
 
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
