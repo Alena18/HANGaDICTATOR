ba3dab884e0e3eedba4e9abfd92f1e1fd33770b8
Alena18 <sun180983@gmail.com>
2023-01-18 22:10:29 +0000

    Add function if word is guessed

--- run.py
+++ run.py
@@ -1,21 +1,22 @@
 import random
+import time
 
 # invitation to a game
 print("\nWelcome to HANGaDICTATOR game by coffeebeanstudio1809@gmail.com")
 # Enter player name
-# def getName():
-#     name = input("Enter your name: ")
-#     return name
+def getName():
+    name = input("Enter your name: ")
+    return name
 
-# def nameText():
-#     while(True):
-#         textName = getName()
-#         if textName.isalpha():
-#             break
-#         else:
-#             print(f"{textName} is not valid. Pease try aglain.")
-#     print("Hello, " + textName.capitalize() + "! Best of Luck!")
-# nameText()
+def nameText():
+    while(True):
+        textName = getName()
+        if textName.isalpha():
+            break
+        else:
+            print(f"{textName} is not valid. Pease try aglain.")
+    print("Hello, " + textName.capitalize() + "! Best of Luck!")
+nameText()
 # Restart game
 def onceagain():
     global play
@@ -29,18 +30,18 @@ def onceagain():
         exit()    
 
 
-# Game start
-# time.sleep(1)
-# print("The game is about to start!\nEnjoy!")
-# time.sleep(2)
+#Game start
+time.sleep(1)
+print("The game is about to start!\nEnjoy!")
+time.sleep(2)
 # game
 # define variable
 def main():
-    words = ["commonwealth"] #, 'democracy','election', 'emancipation',
-    # 'equality', 'fairness', 'freedom', 'honesty', 'humanity',
-    # 'integrity', 'justice', 'liberty', 'minority', 'probity',
-    # 'rectitude', 'rights', 'sincerity', 'suffrage', 'tolerance',
-    # 'uprightness']
+    words = ['commonwealth', 'democracy', 'election', 'emancipation',
+    'equality', 'fairness', 'freedom', 'honesty', 'humanity',
+    'integrity', 'justice', 'liberty', 'minority', 'probity',
+    'rectitude', 'rights', 'sincerity', 'suffrage', 'tolerance',
+    'uprightness']
     word = random.choice(words)
     word = word.upper()
     total_guess = 7
@@ -58,6 +59,17 @@ def main():
                     guessed = guessed[:index]+guess+guessed[index+1:]
             if guessed == word:
                 print(f"Your guessed word: {word}. Well done!!!")
+                time.sleep(1)
+                print("Your dictator is hanged!")
+                time.sleep(1)
+                print("    ______\n"
+                      "   |      |\n"
+                      "   |      |\n"
+                      "   |      |\n"
+                      "   |      O \n"
+                      "   |     /|\ \n"
+                      "   |     / \ \n"
+                      "___|___\n")
                 onceagain()
         else:
             total_guess -= 1
@@ -68,40 +80,4 @@ def main():
         print("\nYou loose.\n")
         print("The correct word is ", word)
         onceagain()
-main()  
-
-# Play game
-# def hang():
-#      guess = input("Enter the HANGaDICTATOR word:" + display + "\n" )
-#      guess = guess.strip()
-#      if len(guess.strip()) == 0 or len(guess.strip()) > 1 or not guess.isalpha():
-#             print("Try a letter\n ")
-#             hang()
-#      elif guess in word:
-#           guessed.extend([guess])
-#           index = word.lower(guess)
-#           word = word[:index] + "_" + word[index + 1:]
-#           print(display + "\n")
-#      elif guess in guessed:
-#           print("Try another letter.\n")
-#      else:
-#           count +=1
-#           if count == 1:
-#              time.sleep(1)
-#              print("    _________\n"
-#                    "   |      \n"
-#                    "   |      \n"
-#                    "   |      \n"
-#                    "   |      \n"
-#                    "   |      \n"
-#                    "   |      \n"
-#                    "___|___   \n"          
-#                  )
-# main()
-
-# hang()
-
-
-
-
-
+main()
