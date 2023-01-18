f4d6158fa64fdb479e01126aa4c4ebbd5583dae3
Alena18 <sun180983@gmail.com>
2023-01-18 23:38:56 +0000

    Add function to check if name entered has more then one character

--- run.py
+++ run.py
@@ -7,12 +7,12 @@ print("\nWelcome to HANGaDICTATOR game by coffeebeanstudio1809@gmail.com")
 def getName():
     name = input("Enter your name: ")
     return name
-
 def nameText():
     while(True):
         textName = getName()
-        if textName.isalpha():
+        if textName.isalpha() and len(textName.strip()) > 1:
             break
+        # If name contains not a letter
         else:
             print(f"{textName} is not valid. Pease try aglain.")
     print("Hello, " + textName.capitalize() + "! Best of Luck!")
@@ -33,9 +33,9 @@ def onceagain():
 #Game start
 time.sleep(1)
 print("The game is about to start!\nEnjoy!")
-time.sleep(2)
-# game
-# define variable
+time.sleep(1)
+# Game
+# Define variable
 def main():
     words = ['commonwealth', 'democracy', 'election', 'emancipation',
     'equality', 'fairness', 'freedom', 'honesty', 'humanity',
@@ -46,17 +46,20 @@ def main():
     word = word.upper()
     total_guess = 7
     guessed = "-"*len(word)
+    print("\nThis is the word to guess to kill a dictator:\n")
     while total_guess !=0:
         print(guessed)
-        guess = input("Enter the HANGaDICTATOR word letter: \n").upper()
+        guess = input("\nEnter the HANGaDICTATOR word letter: \n").upper()
         # Check if a letter that entered is a letter 
         if len(guess.strip()) == 0 or len(guess.strip()) > 1 or not guess.isalpha():
               print("Try a letter\n ")
               continue
         if guess in word:
+            # If the letter guessed
             for index in range(len(word)):
                 if word[index]==guess:
                     guessed = guessed[:index]+guess+guessed[index+1:]
+            # If word guessed        
             if guessed == word:
                 print(f"Your guessed word: {word}. Well done!!!")
                 time.sleep(1)
@@ -71,13 +74,26 @@ def main():
                       "   |     / \ \n"
                       "___|___\n")
                 onceagain()
+        # If enter a wrong letter        
         else:
             total_guess -= 1
             print("Wrong guess.\n")
             print("The attempt(s) left: ", total_guess)
-
+    # If all attempts are failed
     else:
         print("\nYou loose.\n")
         print("The correct word is ", word)
+        print("The dictator is free :( ")
+        time.sleep(1)
+        print("     _____\n"
+              "   ( O   o ) \n"
+              "  /    0    \ \n"
+              " / / |   | \ \ \n"
+              "(``)  | |  (``) \n"
+              "      | | \n"
+              "     /   \ \n"
+              "     |   | \n"
+              "    _|   |_ \n")
+
         onceagain()
 main()
