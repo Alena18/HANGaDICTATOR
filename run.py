bc16c758f3918db6c6d62cb8d3c0671b737e2649
Alena18 <sun180983@gmail.com>
2023-01-27 09:45:37 +0000

    Update code for python for validation

--- run.py
+++ run.py
@@ -3,12 +3,16 @@ import time
 
 # invitation to a game
 print("\nWelcome to HANGaDICTATOR game by coffeebeanstudio1809@gmail.com")
+
+
 # Enter player name
 def getName():
     name = input("Enter your name: ")
     return name
+
+
 def nameText():
-    while(True):
+    while (True):
         textName = getName()
         if textName.isalpha() and len(textName.strip()) > 1:
             break
@@ -16,7 +20,11 @@ def nameText():
         else:
             print(f"{textName} is not valid. Pease try aglain.")
     print("Hello, " + textName.capitalize() + "! Best of Luck!")
+
+
 nameText()
+
+
 # Restart game
 def onceagain():
     global play
@@ -27,39 +35,42 @@ def onceagain():
         main()
     elif play in ["N", "n"]:
         print("Thank you! Hope you enjoyed the game.")
-        exit()    
+        exit()
 
 
-#Game start
+# Game start
 time.sleep(1)
 print("The game is about to start!\nEnjoy!")
 time.sleep(1)
+
+
 # Game
 # Define variable
 def main():
-    words = ['commonwealth', 'democracy', 'election', 'emancipation',
-    'equality', 'fairness', 'freedom', 'honesty', 'humanity',
-    'integrity', 'justice', 'liberty', 'minority', 'probity',
-    'rectitude', 'rights', 'sincerity', 'suffrage', 'tolerance',
-    'uprightness']
+    words = [
+        'commonwealth', 'democracy', 'election', 'emancipation',
+        'equality', 'fairness', 'freedom', 'honesty', 'humanity',
+        'integrity', 'justice', 'liberty', 'minority', 'probity',
+        'rectitude', 'rights', 'sincerity', 'suffrage', 'tolerance',
+        'uprightness']
     word = random.choice(words)
     word = word.upper()
     total_guess = 7
     guessed = "-"*len(word)
     print("\nThis is the word to guess to kill a dictator:\n")
-    while total_guess !=0:
+    while total_guess != 0:
         print(guessed)
         guess = input("\nEnter the HANGaDICTATOR word letter: \n").upper()
-        # Check if a letter that entered is a letter 
-        if len(guess.strip()) == 0 or len(guess.strip()) > 1 or not guess.isalpha():
-              print("Try a letter\n ")
-              continue
+        # Check if a letter that entered is a letter
+        if len(guess.strip()) == 0 or len(guess.strip()) > 1 or not guess.isalpha():  # noqa
+            print("Try a letter\n ")
+            continue
         if guess in word:
             # If the letter guessed
             for index in range(len(word)):
-                if word[index]==guess:
+                if word[index] == guess:
                     guessed = guessed[:index]+guess+guessed[index+1:]
-            # If word guessed        
+            # If word guessed
             if guessed == word:
                 print(f"Your guessed word: {word}. Well done!!!")
                 time.sleep(1)
@@ -70,11 +81,11 @@ def main():
                       "   |      |\n"
                       "   |      |\n"
                       "   |      O \n"
-                      "   |     /|\ \n"
-                      "   |     / \ \n"
+                      "   |     /|\\ \n"
+                      "   |     / \\ \n"
                       "___|___\n")
                 onceagain()
-        # If enter a wrong letter        
+        # If enter a wrong letter
         else:
             total_guess -= 1
             print("Wrong guess.\n")
@@ -87,13 +98,15 @@ def main():
         time.sleep(1)
         print("     _____\n"
               "   ( O   o ) \n"
-              "  /    0    \ \n"
-              " / / |   | \ \ \n"
+              "  /    0    \\ \n"
+              " / / |   | \\ \\ \n"
               "(``)  | |  (``) \n"
               "      | | \n"
-              "     /   \ \n"
+              "     /   \\ \n"
               "     |   | \n"
               "    _|   |_ \n")
 
         onceagain()
+
+
 main()
